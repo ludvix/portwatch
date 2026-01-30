f3e3bfb87fc219b57eb026f8412ecf1d0913e79f
Ludvig <kalbergludvig@outlook.com>
2026-01-30 12:59:29 +0100

    Add argument options to CLI, todo logic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #main py
+import argparse
 
 host = "localhost"
 port_start = 1 
@@ -8,3 +9,14 @@ state_file_path = "ports.json" # File for saving previous scan
 reset_base = False # Start fresh scan file
 json_output = False 
 
+pars = argparse.ArgumentParser("Portwatch CLI")
+
+pars.add_argument("--host", type=str, default="localhost", help="Target host to scan")
+pars.add_argument("--ports", type=str, default="1-1024", help="Port range, format start-end 1-1024")
+pars.add_argument("--timeout", type=float, default=0.5, help="Seconds timer timeout between each port scan")
+pars.add_argument("--state-file", type=str, default="ports.json", help="File to save/read from previous scan")
+pars.add_argument("--reset", action="store_true", help="Start fresh scan file")
+pars.add_argument("--json", action="store_true", help="Output JSON")
+
+args = pars.parse_args()
+print(args)
