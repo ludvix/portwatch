154a96635295b49415287f472326cc5ea6e7d84d
Ludvig <kalbergludvig@outlook.com>
2026-01-30 11:51:41 +0100

    Add neccesary variables

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 #main py
 
+host = "localhost"
+port_start = 1 
+port_end = 1024
+timeout_secs = 0.5 # Seconds timer for per connection attempt
+state_file_path = "ports.json" # File for saving previous scan
+reset_base = False # Start fresh scan file
+json_output = False 
+
