23cb217ac260d306d9929ca4c12e3fe706271d29
Ludvig <kalbergludvig@outlook.com>
2026-03-31 14:47:07 +0200

    Add opened/closed port scan ouput with error handling

--- main.rb
+++ main.rb
@@ -4,8 +4,12 @@ target = ARGV[0]
 port_range = ARGV[1].split('-')
 
 (port_range[0].to_i..port_range[1].to_i).each do |port|
-  Socket.tcp(target, port) {|sock|
-  sock.close_write
-  puts "Hello, #{target} #{port_range}"
-}
+  begin 
+    Socket.tcp(target, port) {|sock|
+    sock.close_write
+    puts "Port #{port}: Open"
+    }
+  rescue
+    puts "Port #{port}: Closed"
+  end
 end
