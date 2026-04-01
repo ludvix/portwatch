af38c23f6449f478189e54c9d48394f396d4f0e8
Ludvig <kalbergludvig@outlook.com>
2026-04-01 20:39:24 +0200

    add timeout to speed up scans

--- main.rb
+++ main.rb
@@ -5,7 +5,7 @@ port_range = ARGV[1].split('-')
 
 (port_range[0].to_i..port_range[1].to_i).each do |port|
   begin 
-    Socket.tcp(target, port) {|sock|
+    Socket.tcp(target, port, connect_timeout: 3) {|sock|
     sock.close_write
     puts "Port #{port}: Open"
     }
